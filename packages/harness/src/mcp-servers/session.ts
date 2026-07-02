627da58700079e534da71c7e642b4c8c66cc3ccd
Jacopo Scazzosi <jacopo@scazzosi.com>
2026-07-02 09:08:25 +0200

    feat: draft of new session fork MCP tool

--- packages/harness/src/mcp-servers/session.ts
+++ packages/harness/src/mcp-servers/session.ts
@@ -3,22 +3,12 @@ import { McpLocalServer } from "@fondamenta/mcp-local";
 import pinetto from 'pinetto';
 import { type CompleteContext } from "../context.js";
 import { type HarnessMcpToolCallContext } from "./types.js";
+import { insertSession } from "../database/tables/sessions.js";
+import { SessionRunner } from "../sessions/runner.js";
+import { selectMessages } from "../database/tables/messages.js";
+import { AgentMessage } from "../models/session/types/messages.js";
 
 export const registerSessionTools = (ctx: CompleteContext, mcp_server: McpLocalServer<HarnessMcpToolCallContext>) => {
-
-  // interface SetModelParams {
-  //   model_id: string;
-  // }
-
-  // mcp_server.addTool<SetModelParams>(
-  //   'set_model',
-  //   'Set Session Model',
-  //   'Configure which model should be used for the specified session.',
-  //   async ({ model_id }, { db, session_id }) => {
-  //     await ctx.managers.sessions.setSessionModel(session_id, model_id, db);
-  //     return [{ type: 'text', text: 'model correctly set' }];
-  //   },
-  // );
 
   interface CompactParams {
     summary: string;
@@ -34,6 +24,47 @@ export const registerSessionTools = (ctx: CompleteContext, mcp_server: McpLocalS
       return [{ type: 'text', text: 'Consolidation successful.' }];
     }),
   );
+
+  // interface ForkParams {
+  //   prompt: string;
+  //   // model_id?: string;
+  // }
+
+  // mcp_server.addTool<ForkParams>(
+  //   'fork',
+  //   'Fork',
+  //   'Use this tool to start a new session from the current one.',
+  //   (async ({ prompt }, { db }) => {
+  //     const { id: session_id } = await insertSession(db, {
+  //       initiator: 'agent',
+  //       created_at: new Date(),
+  //       system_prompt: prompt,
+  //     });
+  //     const runner = new SessionRunner(ctx.init, session_id, session_id);
+  //     await runner.run(db, ctx.managers.mcp.blacklist([
+  //       'mcp_logs_count',
+  //       'mcp_logs_list',
+  //       'mcp_logs_read',
+  //       'mcp_logs_insert',
+  //       'mcp_notes_count',
+  //       'mcp_notes_list',
+  //       'mcp_notes_read',
+  //       'mcp_notes_insert',
+  //       'mcp_notes_update',
+  //       'mcp_notes_append',
+  //       'mcp_anchors_insert',
+  //       'mcp_anchors_select',
+  //       'mcp_anchors_update',
+  //       'mcp_anchors_delete',
+  //     ]));
+  //     const messages = await selectMessages(db, { session_id });
+  //     const result = messages.filter(m => m.role === 'agent')
+  //       .flatMap(m => (m.data as AgentMessage).blocks)
+  //       .filter(b => b.type === 'text')
+  //       .join('\n\n');
+  //     return [{ type: 'text', text: result }];
+  //   }),
+  // );
 
 };
 
